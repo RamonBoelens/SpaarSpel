1fda0e861874661bebf0c034dc3ae79cc7b63466
RamonBoelens <ramonboelens@hotmail.com>
2017-11-09 12:25:50 +0100

    Huisdier Color Change
    The huisdier is changing color depending on his mood level. At this point he changes from happy to sad in 60 seconds

---
+++ Library/Collab/Download/Assets/Scripts/HuisdierMood.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuisdierMood : MonoBehaviour {
+
+    [SerializeField] Renderer Renderer;
+
+    [SerializeField] Color happy;
+    [SerializeField] Color sad;
+
+    private float moodLevel = 100;
+    private float decreaseMoodAmount = 1.67f;
+    private float time;
+
+    private int i = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        // Call the function DecreaseMood every second
+        InvokeRepeating("DecreaseMood", 1.0f, 1.0f);
+
+        time = moodLevel / decreaseMoodAmount;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("Seconds: " + i + ", MoodLevel: " + moodLevel);
+        // Change to color depending on the moodLevel
+        Renderer.material.color = Color.Lerp(sad, happy, moodLevel/time);
+    }
+
+    private void DecreaseMood()
+    {
+        // Decreases MoodLevel with decreaseMoodAmount as long as moodLevel is above 0
+        if (moodLevel > 0)
+        {
+            moodLevel -= decreaseMoodAmount;
+        }
+
+        i++;
+    }
+}
